1961184765c74c294c89b5724841eb01ac7aca46
Yassir Ismaaili <mac@192.168.1.11>
2023-11-15 17:01:38 +0100

    check textures

--- src/parsing/init_texture.c
+++ src/parsing/init_texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_texture.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylarhris <ylarhris@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:47:11 by yismaail          #+#    #+#             */
-/*   Updated: 2023/11/10 21:58:22 by ylarhris         ###   ########.fr       */
+/*   Updated: 2023/11/15 14:05:43 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,44 +27,12 @@ t_image init_texture(t_data *data, int flag)
 	t_image image;
 
 	if (flag == 0)
-	{
 		init_image(data, data->textures.no_texture, &image);
-		// image.img = mlx_xpm_file_to_image(data->mlx->mlx_ptr, data->textures.no_texture, &data->textures.width, &data->textures.height);
-		// if (!image.img)
-		// 	ft_exit(data, "cannot open this image", 1);
-		// image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
-		// if (!image.addr)
-		// 	ft_exit(data, "cannot get data address from texture", 1);
-	}
 	else if (flag == 1)
-	{
-		init_image(data, data->textures.so_texture, &image);
-		// image.img = ml1x_xpm_file_to_image(data->mlx->mlx_ptr, data->textures.no_texture, &data->textures.width, &data->textures.height);
-		// if (!image.img)
-		// 	ft_exit(data, "cannot open this image", 1);
-		// image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
-		// if (!image.addr)
-		// 	ft_exit(data, "cannot get data address from texture", 1);
-	}
-	else if (flag == 3)
-	{
 		init_image(data, data->textures.we_texture, &image);
-		// image.img = mlx_xpm_file_to_image(data->mlx->mlx_ptr, data->textures.no_texture, &data->textures.width, &data->textures.height);
-		// if (!image.img)
-		// 	ft_exit(data, "cannot open this image", 1);
-		// image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
-		// if (!image.addr)
-		// 	ft_exit(data, "cannot get data address from texture", 1);
-	}
-	else if (flag == 4)
-	{
+	else if (flag == 3)
 		init_image(data, data->textures.ea_texture, &image);
-		// image.img = mlx_xpm_file_to_image(data->mlx->mlx_ptr, data->textures.no_texture, &data->textures.width, &data->textures.height);
-		// if (!image.img)
-		// 	ft_exit(data, "cannot open this image", 1);
-		// image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
-		// if (!image.addr)
-			// ft_exit(data, "cannot get data address from texture", 1);
-	}
+	else if (flag == 4)
+		init_image(data, data->textures.so_texture, &image);
 	return (image);
 }
